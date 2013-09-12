88647f735b566a6b0a53b8371f66a6303cf81b3a
nbpro <nbneeraj8@gmail.com>
2013-09-12 20:40:55 +0530

    intial commit

--- index.php
+++ index.php
@@ -80,9 +80,23 @@ $app_info = $facebook->api('/'. AppInfo::appID());
 $app_name = idx($app_info, 'name', '');
 
 ?>
-<!DOCTYPE html>
-
-<iframe src="http://www.tintup.com/neerajpro-1378742850"></iframe> 
-
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<style type="text/css">
+body {
+width:520px;
+margin:0; padding:0; border:0;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html;
+charset=iso-8859-1" />
+</head>
+<body>
+<div id="container">
+<p><iframe src="http://www.tintup.com/neerajpro-1378742850"></iframe></p>
+</div>
+</body>
 </html>
